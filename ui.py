c440d4e115498aa8524502c74f967cba1618938b
Amun1804 <gregoryjoseph@live.com>
2022-04-02 23:01:33 -0400

    updated GUI

--- ui.py
+++ ui.py
@@ -1,14 +1,28 @@
 from tkinter import *
+from data import question_data
 
 THEME_COLOR = "#375362"
 
-window = Tk()
-window.config(padx=50, pady=50, bg=THEME_COLOR)
+class QuizInterface:
 
-canvas = Canvas()
-canvas.grid(row=0, column=0)
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
+        self.score_label = Label(text='score: 0', fg='white', bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
 
-true_button = Button()
+        self.canvas = Canvas(height=250, width=300)
+        self.question = self.canvas.create_text(150, 150, text="some stuff", font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
-window.mainloop()
+        true_img = PhotoImage(file="images/true.png")
+        false_img = PhotoImage(file="images/false.png")
+        self.true_button = Button(image=true_img, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        self.false_button = Button(image=false_img, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
+        self.window.mainloop()
